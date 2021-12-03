130fecf62332bb9feb5bab4a3ef913c50ce4fe91
elisei <brunoelisei@o2ti.com>
2021-12-03 08:57:24 -0300

    O2TI 🖤 Magento
    abstracting methods

--- Plugin/CheckoutAdvancedFieldsCheckout.php
+++ Plugin/CheckoutAdvancedFieldsCheckout.php
@@ -10,6 +10,9 @@ namespace O2TI\AdvancedFieldsCheckout\Plugin;
 
 use Magento\Checkout\Block\Checkout\LayoutProcessor;
 use O2TI\AdvancedFieldsCheckout\Helper\Config;
+use O2TI\AdvancedFieldsCheckout\Model\AddtionalClasses;
+use O2TI\AdvancedFieldsCheckout\Model\Autocomplete;
+use O2TI\AdvancedFieldsCheckout\Model\Placeholder;
 
 /**
  *  CheckoutAdvancedFieldsCheckout - Change Components.
@@ -22,12 +25,36 @@ class CheckoutAdvancedFieldsCheckout
     protected $config;
 
     /**
-     * @param Config $config
+     * @var AddtionalClasses
+     */
+    protected $addtionalClasses;
+
+    /**
+     * @var Autocomplete
+     */
+    protected $autocomplete;
+
+    /**
+     * @var Placeholder
+     */
+    protected $placeholder;
+
+    /**
+     * @param Config           $config
+     * @param AddtionalClasses $addtionalClasses
+     * @param Autocomplete     $autocomplete
+     * @param Placeholder      $placeholder
      */
     public function __construct(
-        Config $config
+        Config $config,
+        AddtionalClasses $addtionalClasses,
+        Autocomplete $autocomplete,
+        Placeholder $placeholder
     ) {
         $this->config = $config;
+        $this->addtionalClasses = $addtionalClasses;
+        $this->autocomplete = $autocomplete;
+        $this->placeholder = $placeholder;
     }
 
     /**
@@ -44,13 +71,13 @@ class CheckoutAdvancedFieldsCheckout
             // phpcs:ignore
             $createAccountFields = &$jsLayout['components']['checkout']['children']['steps']['children']['identification-step']['children']['identification']['children']['createAccount']['children']['create-account-fieldset']['children'];
             if ($this->config->isEnabledClasses()) {
-                $createAccountFields = $this->changeClassesFields($createAccountFields);
+                $createAccountFields = $this->addtionalClasses->setAddtionalClasses($createAccountFields);
             }
             if ($this->config->isEnabledAutocomplete()) {
-                $createAccountFields = $this->setAutocompleteFields($createAccountFields);
+                $createAccountFields = $this->autocomplete->setAutocomplete($createAccountFields);
             }
             if ($this->config->isEnabledPlaceholder()) {
-                $createAccountFields = $this->setPlaceholderFields($createAccountFields);
+                $createAccountFields = $this->placeholder->setPlaceholder($createAccountFields);
             }
         }
 
@@ -70,13 +97,13 @@ class CheckoutAdvancedFieldsCheckout
             // phpcs:ignore
             $shippingFields = &$jsLayout['components']['checkout']['children']['steps']['children']['shipping-step']['children']['shippingAddress']['children']['shipping-address-fieldset']['children'];
             if ($this->config->isEnabledClasses()) {
-                $shippingFields = $this->changeClassesFields($shippingFields);
+                $shippingFields = $this->addtionalClasses->setAddtionalClasses($shippingFields);
             }
             if ($this->config->isEnabledAutocomplete()) {
-                $shippingFields = $this->setAutocompleteFields($shippingFields);
+                $shippingFields = $this->autocomplete->setAutocomplete($shippingFields);
             }
             if ($this->config->isEnabledPlaceholder()) {
-                $shippingFields = $this->setPlaceholderFields($shippingFields);
+                $shippingFields = $this->placeholder->setPlaceholder($shippingFields);
             }
         }
 
@@ -97,13 +124,13 @@ class CheckoutAdvancedFieldsCheckout
             if (isset($payment['children']['form-fields'])) {
                 $billingFields = &$payment['children']['form-fields']['children'];
                 if ($this->config->isEnabledClasses()) {
-                    $billingFields = $this->changeClassesFields($billingFields);
+                    $billingFields = $this->addtionalClasses->setAddtionalClasses($billingFields);
                 }
                 if ($this->config->isEnabledAutocomplete()) {
-                    $billingFields = $this->setAutocompleteFields($billingFields);
+                    $billingFields = $this->autocomplete->setAutocomplete($billingFields);
                 }
                 if ($this->config->isEnabledPlaceholder()) {
-                    $billingFields = $this->setPlaceholderFields($billingFields);
+                    $billingFields = $this->placeholder->setPlaceholder($billingFields);
                 }
             }
         }
@@ -112,13 +139,13 @@ class CheckoutAdvancedFieldsCheckout
             // phpcs:ignore
             $billingAddressOnPage = &$jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['afterMethods']['children']['billing-address-form']['children']['form-fields']['children'];
             if ($this->config->isEnabledClasses()) {
-                $billingAddressOnPage = $this->changeClassesFields($billingAddressOnPage);
+                $billingAddressOnPage = $this->addtionalClasses->setAddtionalClasses($billingAddressOnPage);
             }
             if ($this->config->isEnabledAutocomplete()) {
-                $billingAddressOnPage = $this->setAutocompleteFields($billingAddressOnPage);
+                $billingAddressOnPage = $this->autocomplete->setAutocomplete($billingAddressOnPage);
             }
             if ($this->config->isEnabledPlaceholder()) {
-                $billingAddressOnPage = $this->setPlaceholderFields($billingAddressOnPage);
+                $billingAddressOnPage = $this->placeholder->setPlaceholder($billingAddressOnPage);
             }
         }
         // phpcs:ignore
@@ -126,195 +153,17 @@ class CheckoutAdvancedFieldsCheckout
             // phpcs:ignore
             $billingAddressOnPage = &$jsLayout['components']['checkout']['children']['steps']['children']['billing-step']['children']['payment']['children']['beforeMethods']['children']['billing-address-form']['children']['form-fields']['children'];
             if ($this->config->isEnabledClasses()) {
-                $billingAddressOnPage = $this->changeClassesFields($billingAddressOnPage);
+                $billingAddressOnPage = $this->addtionalClasses->setAddtionalClasses($billingAddressOnPage);
             }
             if ($this->config->isEnabledAutocomplete()) {
-                $billingAddressOnPage = $this->setAutocompleteFields($billingAddressOnPage);
+                $billingAddressOnPage = $this->autocomplete->setAutocomplete($billingAddressOnPage);
             }
             if ($this->config->isEnabledPlaceholder()) {
-                $billingAddressOnPage = $this->setPlaceholderFields($billingAddressOnPage);
+                $billingAddressOnPage = $this->placeholder->setPlaceholder($billingAddressOnPage);
             }
         }
 
         return $jsLayout;
-    }
-
-    /**
-     * Change Classes in Fields.
-     *
-     * @param array $fields
-     *
-     * @return array
-     */
-    public function changeClassesFields(array $fields): array
-    {
-        foreach ($fields as $key => $data) {
-            $oldClass = null;
-            if (in_array('config', $fields[$key])) {
-                if ($fields[$key]['config']) {
-                    $newClass = $this->config->getAddtionalClassesForField($key);
-                    if ($newClass) {
-                        if (in_array('additionalClasses', $fields[$key]['config'])) {
-                            $oldClass = $fields[$key]['config']['additionalClasses'];
-                        }
-                        $fields[$key]['config']['additionalClasses'] = $oldClass.' '.$newClass;
-                    }
-                    if ($key === 'street') {
-                        $fields = $this->setClassesToStreet($fields, $key);
-                    }
-                }
-            }
-        }
-
-        return $fields;
-    }
-
-    /**
-     * Set Components Classes in Street Lines.
-     *
-     * @param array  $fields
-     * @param string $field
-     *
-     * @return array
-     */
-    public function setClassesToStreet(array $fields, string $field): array
-    {
-        $oldClass = null;
-        foreach ($fields[$field]['children'] as $arrayPosition => $streetLine) {
-            $streetKey = 'street_'.$arrayPosition;
-            $newClass = $this->config->getAddtionalClassesForField($streetKey);
-            if ($newClass) {
-                if (in_array('additionalClasses', $fields[$field]['children'][$arrayPosition]['config'])) {
-                    $oldClass = $fields[$field]['children'][$arrayPosition]['config']['additionalClasses'];
-                }
-                $fields[$field]['children'][$arrayPosition]['config']['additionalClasses'] = $oldClass.' '.$newClass;
-            }
-        }
-
-        return $fields;
-    }
-
-    /**
-     * Set Components Autocomplete in Fields.
-     *
-     * @param array $fields
-     *
-     * @return array
-     */
-    public function setAutocompleteFields(array $fields): array
-    {
-        foreach ($fields as $key => $data) {
-            if (in_array('config', $fields[$key])) {
-                if ($fields[$key]['config']) {
-                    $autocomplete = $this->config->getAutocompleteForField($key);
-                    if ($autocomplete) {
-                        $fields[$key]['config']['autocomplete'] = $autocomplete;
-                        if ($fields[$key]['config']['elementTmpl'] === 'ui/form/element/input') {
-                            $fields[$key]['config']['elementTmpl'] = 'O2TI_AdvancedFieldsCheckout/form/element/input';
-                        }
-                        if ($fields[$key]['config']['elementTmpl'] === 'ui/form/element/select') {
-                            $fields[$key]['config']['elementTmpl'] = 'O2TI_AdvancedFieldsCheckout/form/element/select';
-                        }
-                        if ($fields[$key]['config']['elementTmpl'] === 'ui/form/element/password') {
-                            // phpcs:ignore
-                            $fields[$key]['config']['elementTmpl'] = 'O2TI_AdvancedFieldsCheckout/form/element/password';
-                        }
-                        // phpcs:ignore
-                        if ($fields[$key]['config']['elementTmpl'] === 'O2TI_CheckoutIdentificationStep/form/element/password') {
-                            // phpcs:ignore
-                            $fields[$key]['config']['elementTmpl'] = 'O2TI_AdvancedFieldsCheckout/form/element/O2TI/password';
-                        }
-                    }
-                    if ($key === 'street') {
-                        $fields = $this->setAutocompleteToStreet($fields, $key);
-                    }
-                }
-            }
-        }
-
-        return $fields;
-    }
-
-    /**
-     * Set Components Autocomplete in Street Lines.
-     *
-     * @param array  $fields
-     * @param string $field
-     *
-     * @return array
-     */
-    public function setAutocompleteToStreet(array $fields, string $field): array
-    {
-        foreach ($fields[$field]['children'] as $arrayPosition => $streetLine) {
-            $streetKey = 'street_'.$arrayPosition;
-            $autocomplete = $this->config->getAutocompleteForField($streetKey);
-            if ($autocomplete) {
-                if ($fields[$field]['children'][$arrayPosition]['config']['elementTmpl'] === 'ui/form/element/input') {
-                    // phpcs:ignore
-                    $fields[$field]['children'][$arrayPosition]['config']['elementTmpl'] = 'O2TI_AdvancedFieldsCheckout/form/element/input';
-                }
-                // phpcs:ignore
-                if ($fields[$field]['children'][$arrayPosition]['config']['elementTmpl'] === 'O2TI_AdvancedStreetAddress/form/element/number') {
-                    // phpcs:ignore
-                    $fields[$field]['children'][$arrayPosition]['config']['elementTmpl'] = 'O2TI_AdvancedFieldsCheckout/form/element/number';
-                }
-
-                $fields[$field]['children'][$arrayPosition]['config']['autocomplete'] = $autocomplete;
-            }
-        }
-
-        return $fields;
-    }
-
-    /**
-     * Set Components Placeholder in Fields.
-     *
-     * @param array $fields
-     *
-     * @return array
-     */
-    public function setPlaceholderFields(array $fields): array
-    {
-        foreach ($fields as $key => $data) {
-            if (in_array('label', $fields[$key])) {
-                if ($fields[$key]['label']) {
-                    $placeholder = $this->config->getPlaceholderForField($key);
-                    $label = $fields[$key]['label'];
-                    $fields[$key]['config']['placeholder'] = __($label);
-                    if ($placeholder) {
-                        $fields[$key]['config']['placeholder'] = __($placeholder);
-                    }
-                    if ($key === 'street') {
-                        $fields = $this->setPlaceholderToStreet($fields, $key);
-                    }
-                }
-            }
-        }
-
-        return $fields;
-    }
-
-    /**
-     * Set Components Placeholder in Street Lines.
-     *
-     * @param array  $fields
-     * @param string $field
-     *
-     * @return array
-     */
-    public function setPlaceholderToStreet(array $fields, string $field): array
-    {
-        foreach ($fields[$field]['children'] as $arrayPosition => $streetLine) {
-            $streetKey = 'street_'.$arrayPosition;
-            $placeholder = $this->config->getPlaceholderForField($streetKey);
-            $label = $fields[$field]['children'][$arrayPosition]['label'];
-            $fields[$field]['children'][$arrayPosition]['config']['placeholder'] = __($label);
-            if ($placeholder) {
-                $fields[$field]['children'][$arrayPosition]['config']['placeholder'] = __($placeholder);
-            }
-        }
-
-        return $fields;
     }
 
     /**
